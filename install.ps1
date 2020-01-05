82279ee6acec7f446259e4d98e84c1e44972aa4f
Tester <123@123.com>
2020-01-05 12:46:30 -0500

    Add install script

---
+++ install.ps1
@@ -0,0 +1,7 @@
+﻿# install chocolatey- run next line without the # in powershell (run as admin) on its own if you don't have chocolatey  See https://chocolatey.org/install
+#Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco upgrade gitextensions -y
+choco upgrade putty.install -y
+choco upgrade p4merge -y
+choco upgrade gpg4win -y
