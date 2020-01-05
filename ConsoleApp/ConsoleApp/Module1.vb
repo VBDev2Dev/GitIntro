dfad61926186f8a50732fa0ac493c2316d74db17
Tester <123@123.com>
2020-01-05 13:23:18 -0500

    Changed in master

--- ConsoleApp/ConsoleApp/Module1.vb
+++ ConsoleApp/ConsoleApp/Module1.vb
@@ -5,7 +5,7 @@
         Console.WriteLine("Hi git step 2.")
         Console.WriteLine("Hi git step 3.")
         Console.WriteLine("Hi git step 4.")
-        Console.WriteLine("Hi git step 5.")
+        Console.WriteLine("Hi git step 5 Modified in master.")
 
     End Sub
 
